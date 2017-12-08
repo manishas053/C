ab8322612ea81b792f84652e261e64f5970e1ba9
manishas053 <manishas053@gmail.com>
2017-12-08 22:36:10 +0530

    Changed The Limit In For Loop

--- Chtholly's-request/zcy.c
+++ Chtholly's-request/zcy.c
@@ -9,7 +9,7 @@
 int main(){
   int k, p, output, i, num, rem, reverse, sum = 0, count =0;
   scanf("%d %d", &k, &p);
-  for(i = 11; i <= 1000; i ++){
+  for(i = 11; i <= p+1000; i ++){
     reverse = 0;
     num = i;
     for(num = i; num > 0; num = num / 10){

2eb44e55a7b93d463adca92d0f59a16b4ca1ea6e
manishas053 <manishas053@gmail.com>
2017-12-08 21:42:45 +0530

    Added Comment

--- Chtholly's-request/zcy.c
+++ Chtholly's-request/zcy.c
@@ -1,3 +1,9 @@
+//###################################################################################################
+// Input line has 2 integers k and p                                                                #
+// Find sum of first k zcy numbers and output this sum modulo p                                     #
+// If a number is palindrome and the number of digits in it is even then it is called a zcy number  #
+//###################################################################################################
+
 #include <stdio.h>
 
 int main(){

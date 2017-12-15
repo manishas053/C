d4416f50dc1e2a99718fe636d12ae8ebac15684e
manishas053 <manishas053@gmail.com>
2017-12-15 22:12:50 +0530

    Indendation

--- Boxes-Packing/pack.c
+++ Boxes-Packing/pack.c
@@ -1,41 +1,32 @@
 
 #include <stdio.h>
 
-void bubble_sort(int list[], int n)
-{
+void bubble_sort(int list[], int n){
   int c, d, t;
 
-  for (c = 0 ; c < ( n - 1 ); c++)
-  {
-    for (d = 0 ; d < n - c - 1; d++)
-    {
-      if (list[d] > list[d+1])
-      {
-        t         = list[d];
-        list[d]   = list[d+1];
+  for (c = 0 ; c < ( n - 1 ); c++)  {
+    for (d = 0 ; d < n - c - 1; d++)    {
+      if (list[d] > list[d+1]){
+        t = list[d];
+        list[d] = list[d+1];
         list[d+1] = t;
       }
     }
   }
 }
+
 int c[5005];
-int main()
-{
+
+int main(){
     int a[5005], n, i, d,j, swap;
-
     scanf("%d", &n);
-
     for (i = 0; i < n; i++)
         scanf("%d", &a[i]);
-
     bubble_sort(a, n);
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++) {
             if(a[i]==a[j])
                 c[i]++;
-
         }
 
     }
